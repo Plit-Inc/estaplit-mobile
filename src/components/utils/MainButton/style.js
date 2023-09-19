5b5a36a36554f6ba5a07f3ed77756cc49e6596bd
Joao Madruga <joaop050702@hotmail.com>
2023-09-19 01:20:21 -0300

    Created input and date toggle
    List of changes:
    - Created date toggle
    - Created input with react native paper
    - Added new fonts
    - Fixed button for touchableopacity instead of view

--- src/components/utils/MainButton/style.js
+++ src/components/utils/MainButton/style.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components/native';
 import * as Constants from "../../../constants/index";
 
-export const MainButtonStyle = styled.View`
+export const MainButtonStyle = styled.TouchableOpacity`
     flex-direction: row;
     gap: 8px;
     align-items: center;
+    justify-content: space-between;
     background-color: ${Constants.buttonConfig.Default.Primary.Default.BackgroundColor};
     padding: ${Constants.buttonConfig.Default.Primary.Default.Padding};
     border-radius: ${Constants.buttonConfig.Default.Primary.Default.Radius};
